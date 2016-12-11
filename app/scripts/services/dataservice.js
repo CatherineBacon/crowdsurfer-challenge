589b2a70e0c9a429f3fc752b05c7a73e60e650dc
catherine <catherine.bacon@gmail.com>
2016-12-11 13:38:58 +0000

    Load from copy of data file

--- app/scripts/services/dataservice.js
+++ app/scripts/services/dataservice.js
@@ -11,7 +11,7 @@ angular.module('crowdsurferApp')
   .service('dataService', ['$http', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     this.fetch = function(cb) {
-      $http.get('https://s3-eu-west-1.amazonaws.com/crowdsurfer-json-dumps/blockchain-projects.json')
+      $http.get('/blockchain-projects.json')
         .then(function(response){
           cb(response.data);
         });
