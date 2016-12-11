220ae496539fcc184f7c62ad5551e18b2c6321b6
catherine <catherine.bacon@gmail.com>
2016-12-11 15:26:47 +0000

    Fix relative paths for dist

--- app/scripts/services/dataservice.js
+++ app/scripts/services/dataservice.js
@@ -11,7 +11,8 @@ angular.module('crowdsurferApp')
   .service('dataService', ['$http', function ($http) {
     // AngularJS will instantiate a singleton by calling "new" on this function
     this.fetch = function(cb) {
-      $http.get('/blockchain-projects.json')
+      var path = window.location.origin + window.location.pathname;
+      $http.get(path + 'blockchain-projects.json')
         .then(function(response){
           cb(response.data);
         });
